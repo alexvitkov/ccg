3591512ee7cc0b11e7dc90f8f63e9b626613bc7e
Alex Vitkov <alexvitkov98@gmail.com>
2020-11-09 16:21:14 +0200

    register users now works

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
 const { MongoClient } = require("mongodb");
+const express = require('express');
 
 // Connection URI
 const uri =
-  "mongodb+srv://127.0.0.1:27017/?poolSize=20&w=majority";
+  "mongodb://127.0.0.1:27017/?poolSize=20&w=majority";
 
 // Create a new MongoClient
 const client = new MongoClient(uri);
 
+var db;
+var users;
+
 async function run() {
   try {
-    // Connect the client to the server
     await client.connect();
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
+    db = client.db("admin");
+    users = db.collection("users");
+    await db.command({ ping: 1 });
     console.log("Connected successfully to server");
+
+    console.log(await register('mi6o', '1234'));
+    console.log(await register('go6o', '1234'));
+
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
 run().catch(console.dir);
+
+
+async function findUser(username) {
+  user = await users.findOne({username:username});
+  return user;
+}
+
+
+async function register(username, password) {
+  const existingUser = await findUser(username);
+  if (existingUser)
+    return false;
+  
+  await users.insertOne({
+    username: username,
+    password: password
+  });
+  return true; // TODO check if insertone fails
+}
+
+
 
