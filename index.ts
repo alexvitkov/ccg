245faab706bddf7d31fd0d96ba0df1ea0334698b
Alex Vitkov <alexvitkov98@gmail.com>
2020-11-16 08:00:59 +0200

    fixed lobby bugs

--- index.ts
+++ index.ts
@@ -24,36 +24,36 @@ expressApp.use(cookieParser());
 
 
 class Session {
-  userId: string;
-  username: string;
-  sock: ws;
-  lobby: Lobby;
+	userId: string;
+	username: string;
+	sock: ws;
+	lobby: Lobby;
 
-  constructor(userInDb: any) {
-    this.userId = userInDb.id.toString('base64');
-    this.username = userInDb.username;
-  }
-  send(msg) {
-    if (this.sock) { this.sock.send(msg); }
-  }
+	constructor(userInDb: any) {
+		this.userId = userInDb.id.toString('base64');
+		this.username = userInDb.username;
+	}
+	send(msg) {
+		if (this.sock) { this.sock.send(msg); }
+	}
 }
 
 class Lobby {
-  id: string;
-  name: string;
-  creatorSession: Session;
-  otherPlayer: Session;
+	id: string;
+	name: string;
+	creatorSession: Session;
+	otherPlayer: Session;
 
-  constructor(creatorSession: Session) {
-    this.creatorSession = creatorSession;
-    this.name = creatorSession.username + "'s lobby";
-    
-    do {
-      this.id = crypto.randomBytes(8).toString('base64');
-    } while (this.id in lobbies2);
+	constructor(creatorSession: Session) {
+		this.creatorSession = creatorSession;
+		this.name = creatorSession.username + "'s lobby";
 
-    lobbies2[this.id] = this;
-  }
+		do {
+			this.id = crypto.randomBytes(8).toString('base64');
+		} while (this.id in lobbies2);
+
+		lobbies2[this.id] = this;
+	}
 }
 
 // const activeSessions: {[session: string]: Session } = {};
@@ -61,278 +61,299 @@ const activeSessions2: {[userId: string]: Session } = {};
 const lobbies2: {[lobbyId: string]: Lobby} = {};
 
 async function runMongo() {
-  await mongoClient.connect();
-  db = mongoClient.db("ccg");
-  users = db.collection("users");
-  sessionsColl = db.collection("sessions");
-  await db.command({ ping: 1 });
-  console.log("Connected to MongoDB server");
+	await mongoClient.connect();
+	db = mongoClient.db("ccg");
+	users = db.collection("users");
+	sessionsColl = db.collection("sessions");
+	await db.command({ ping: 1 });
+	console.log("Connected to MongoDB server");
 }
 
 
 async function getSessionByCookie(cookie: string): Promise<Session> {
-  const sessionFromDb = await sessionsColl.findOne({cookie: cookie});
-  const userId: Buffer = sessionFromDb.userId;
-  const userId64 = userId.toString('base64');
+	const sessionFromDb = await sessionsColl.findOne({cookie: cookie});
+	const userId: Buffer = sessionFromDb.userId;
+	const userId64 = userId.toString('base64');
 
-  if (userId64 in sessionsColl) {
-    console.log(1, activeSessions2[userId64]);
-    return activeSessions2[userId64];
-  }
-  else {
-    const user = await users.findOne({id: userId});
-    if (!user) {
-      return null;
-    }
-    else {
-      activeSessions2[userId64] = new Session(user)
-      console.log(2, activeSessions2[userId64]);
-      return activeSessions2[userId64];
-    }
-  }
+	if (userId64 in activeSessions2) {
+		return activeSessions2[userId64];
+	}
+	else {
+		const user = await users.findOne({id: userId});
+		if (!user) {
+			return null;
+		}
+		else {
+			console.log("genSessionByCookie: creating session for " + user.username);
+			activeSessions2[userId64] = new Session(user)
+			return activeSessions2[userId64];
+		}
+	}
 }
 
 
 function runExpress() {
-  const wsServer = new ws.Server({ noServer: true });
+	const wsServer = new ws.Server({ noServer: true });
 
-  const server = expressApp.listen(PORT, () => {
-    console.log(`Express.js server started at port ${PORT}`)
-  });
-  server.on('upgrade', (req, tcpSocket, head) => {
-    const cookies = cookie.parse(req.headers.cookie);
-    const sessionCookie = cookies.session;
+	const server = expressApp.listen(PORT, () => {
+		console.log(`Express.js server started at port ${PORT}`)
+	});
+	server.on('upgrade', (req, tcpSocket, head) => {
+		const cookies = cookie.parse(req.headers.cookie);
+		const sessionCookie = cookies.session;
 
-    wsServer.handleUpgrade(req, tcpSocket, head, async sock => {
-      if (typeof sessionCookie === 'string') {
-        (sock as any).session = await getSessionByCookie(sessionCookie);
-      }
-      wsServer.emit('connection', sock, req);
-    });
-  });
+		wsServer.handleUpgrade(req, tcpSocket, head, async sock => {
+			if (typeof sessionCookie === 'string') {
+				(sock as any).session = await getSessionByCookie(sessionCookie);
+			}
+			wsServer.emit('connection', sock, req);
+		});
+	});
 
-  wsServer.on('connection', sock => {
-    const session = (sock as any).session;
-    if (!session) {
-      sock.close(4001, 'Invalid session');
-    }
-    else {
-      session.sock = sock;
-      sock.on('message', message => {
-        var msg: object = null;
-        try {
-          msg = JSON.parse(message as string);
-        } catch {
-          sock.close(4002, 'Invalid message');
-        }
-        if (typeof msg !== 'object') {
-          sock.close(4003, 'Invalid message');
-        }
-        else if (sock.readyState === sock.OPEN) {
-          if (!handleWsMessage(session, msg))
-            sock.close(4004, 'Invalid message');
-        }
-      });
-    }
-  });
+	wsServer.on('connection', sock => {
+		const session = (sock as any).session;
+		if (!session) {
+			sock.close(4001, 'Invalid session');
+		}
+		else {
+			session.sock = sock;
+			sock.on('message', message => {
+				var msg: object = null;
+				try {
+					msg = JSON.parse(message as string);
+				} catch {
+					sock.close(4002, 'Invalid message');
+				}
+				if (typeof msg !== 'object') {
+					sock.close(4003, 'Invalid message');
+				}
+				else if (sock.readyState === sock.OPEN) {
+					if (!handleWsMessage(session, msg))
+						sock.close(4004, 'Invalid message');
+				}
+			});
+		}
+	});
 }
 
 
 function handleWsMessage(session: Session, message: {[key:string]:any}) {
-  console.log(`WS[${session.username}]: `, message);
+	console.log(`WS[${session.username}]: `, message);
 
-  switch (message.message) {
-    case 'createLobby': {
-      if (!session.lobby) {
-        session.lobby = new Lobby(session);
-      }
-      refreshLobbiesForPlayer(session);
-      break;
-    }
+	switch (message.message) {
+		case 'createLobby': {
+			if (!session.lobby) {
+				session.lobby = new Lobby(session);
+			}
+			refreshLobbiesForPlayer(session);
+			break;
+		}
 
-    case 'listLobbies': {
-      refreshLobbiesForPlayer(session);
-      break;
-    }
+		case 'listLobbies': {
+			refreshLobbiesForPlayer(session);
+			break;
+		}
 
-    case 'leaveLobby': {
-      if (session.lobby) {
-        const lobby = session.lobby;
+		case 'joinLobby': {
+			if (session.lobby !== null) {
+				leaveLobby(session);
+			}
+			const lobby = lobbies2[message.lobby];
+			if (lobby && !lobby.otherPlayer && lobby.creatorSession !== session) {
+				lobby.otherPlayer = session;
+				session.lobby = lobby;
+			}
+			refreshLobbiesForPlayer(session);
+			refreshLobbiesForPlayer(lobby.creatorSession);
+			break;
+		}
 
-        if (lobby.creatorSession === session) {
-          delete lobbies2[lobby.id];
-          if (lobby.otherPlayer) {
-            lobby.otherPlayer.lobby = null;
-            refreshLobbiesForPlayer(session.lobby.otherPlayer);
-          }
-        }
-        else {
-          session.lobby = null;
-          refreshLobbiesForPlayer(lobby.creatorSession);
-        }
-      }
-      session.lobby = null;
-      refreshLobbiesForPlayer(session);
-      break;
-    }
+		case 'leaveLobby': {
+			leaveLobby(session);
+			break;
+		}
 
-    default: {
-      return false;
-    }
-  }
-  return true;
+		default: {
+			return false;
+		}
+	}
+	return true;
 }
 
 
+function leaveLobby(session: Session) {
+	if (session.lobby) {
+		const lobby = session.lobby;
+
+		if (lobby.creatorSession === session) {
+			delete lobbies2[lobby.id];
+			if (lobby.otherPlayer) {
+				lobby.otherPlayer.lobby = null;
+				refreshLobbiesForPlayer(session.lobby.otherPlayer);
+			}
+		}
+		else {
+			session.lobby = null;
+			lobby.otherPlayer = null;
+			refreshLobbiesForPlayer(lobby.creatorSession);
+		}
+	}
+	session.lobby = null;
+	refreshLobbiesForPlayer(session);
+}
+
 function refreshLobbiesForPlayer(session: Session) {
-  const l = Object.values(lobbies2).map(lobby => { return {
-    id: lobby.id,
-    name: lobby.name,
-    players: lobby.otherPlayer ? 2 : 1
-  }});
-  session.send(JSON.stringify({
-    message: 'listLobbies',
-    success: true,
-    lobbyId: session.lobby?.id,
-    lobbies: l
-  }));
+	const l = Object.values(lobbies2).map(lobby => { return {
+		id: lobby.id,
+		name: lobby.name,
+		players: lobby.otherPlayer ? 2 : 1
+	}});
+	const lobbyId = (session.lobby?.id);
+	session.send(JSON.stringify({
+		message: 'listLobbies',
+		success: true,
+		lobbyId: lobbyId,
+		lobbies: l
+	}));
 }
 
 async function register(username: string, password: string) {
-  const existingUser = await users.findOne({username:username});
-  if (existingUser)
-    return null;
+	const existingUser = await users.findOne({username:username});
+	if (existingUser)
+		return null;
 
-  if (username.length < 3 || username.length > 16)
-    return null;
-  if (!username.match(/^[a-zA-Z0-9.\-_]*$/))
-    return null;
-  if (password.length < 8 )
-    return null;
+	if (username.length < 3 || username.length > 16)
+		return null;
+	if (!username.match(/^[a-zA-Z0-9.\-_]*$/))
+		return null;
+	if (password.length < 8 )
+		return null;
 
-  var userId: Buffer;
-  do {
-    userId = crypto.randomBytes(4);
-  } while (await users.findOne({ userId: userId }));
+	var userId: Buffer;
+	do {
+		userId = crypto.randomBytes(4);
+	} while (await users.findOne({ userId: userId }));
 
-  await users.insertOne({
-    username: username,
-    password: password,
-    id: userId
-  });
-  return users.findOne({id:userId});
+	await users.insertOne({
+		username: username,
+		password: password,
+		id: userId
+	});
+	return users.findOne({id:userId});
 }
 
 async function createSession(userId: Buffer, password: string) {
-  const user = await users.findOne({id: userId, password: password});
-  if (!user) {
-    console.log('no user', userId, password)
-    return null;
-  }
+	const user = await users.findOne({id: userId, password: password});
+	if (!user) {
+		console.log('no user', userId, password)
+		return null;
+	}
 
-  var sessionCookie: string;
-  do {
-    const cookieBytes = crypto.randomBytes(24);
-    sessionCookie = cookieBytes.toString('hex');
-  } while (await sessionsColl.findOne({ cookie: sessionCookie }));
+	var sessionCookie: string;
+	do {
+		const cookieBytes = crypto.randomBytes(24);
+		sessionCookie = cookieBytes.toString('hex');
+	} while (await sessionsColl.findOne({ cookie: sessionCookie }));
 
-  users.updateOne(
-    {userId: userId},
-    { $push: { sessions: sessionCookie } }
-  );
-  sessionsColl.insertOne({cookie: sessionCookie, userId: userId});
-  activeSessions2[userId.toString('base64')] = new Session(user);
-  return sessionCookie;
+	users.updateOne(
+		{userId: userId},
+		{ $push: { sessions: sessionCookie } }
+	);
+	sessionsColl.insertOne({cookie: sessionCookie, userId: userId});
+	console.log("createSession: creating session for " + user.username);
+	activeSessions2[userId.toString('base64')] = new Session(user);
+	return sessionCookie;
 }
 
 
 expressApp.get('/', async (req, res) => {
-  const sessionCookie = req.cookies.session;
+	const sessionCookie = req.cookies.session;
 
-  if (typeof sessionCookie === 'string') {
-    const sessionDb = await sessionsColl.findOne({cookie: sessionCookie});
-    if (!sessionDb) {
-      // Invalid session cookie
-      res.cookie('session', '', {maxAge: 0, sameSite: 'strict'});
-    }
-    else {
-      const userId = sessionDb.userId;
-      const userId64 = userId.toString('base64');
-      const user = await users.findOne({id: userId});
+	if (typeof sessionCookie === 'string') {
+		const sessionDb = await sessionsColl.findOne({cookie: sessionCookie});
+		if (!sessionDb) {
+			// Invalid session cookie
+			res.cookie('session', '', {maxAge: 0, sameSite: 'strict'});
+		}
+		else {
+			const userId = sessionDb.userId;
+			const userId64 = userId.toString('base64');
+			const user = await users.findOne({id: userId});
 
-      if (!(userId64 in activeSessions2))
-        activeSessions2[userId64] = new Session(user);
+			if (!(userId64 in activeSessions2))
+				console.log("get(/): creating session for " + user.username);
+				activeSessions2[userId64] = new Session(user);
 
-      const session = activeSessions2[userId64];
+			const session = activeSessions2[userId64];
 
-      res.render('index', {
-        loggedIn: true,
-        username: session.username,
-        inLobby: !!session.lobby,
-        lobbyId: session.lobby ? session.lobby.id : 0,
-      });
-      return;
-    }
-  }
+			res.render('index', {
+				loggedIn: true,
+				username: session.username,
+				inLobby: !!session.lobby,
+				lobbyId: session.lobby ? session.lobby.id : 0,
+			});
+			return;
+		}
+	}
 
-  res.render('index', {
-    loggedIn: false,
-  });
+	res.render('index', {
+		loggedIn: false,
+	});
 
 });
 
 
 expressApp.post('/register2', async (req: express.Request, res: express.Response) => {
-  const { username, password }  = req.body;
+	const { username, password }  = req.body;
 
-  res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Content-Type', 'application/json');
 
-  if (typeof username !== 'string' || typeof password !== 'string') {
-    res.status(400).send('{}');
-    return;
-  }
+	if (typeof username !== 'string' || typeof password !== 'string') {
+		res.status(400).send('{}');
+		return;
+	}
 
-  const result = <any>await register(username, password);
-  if (result) {
-    const cookie = await createSession(result.id, password);
-    res.send(JSON.stringify({
-      username: username,
-      session: cookie
-    }));
-  }
-  else {
-    res.status(400).send('{}');
-  }
+	const result = <any>await register(username, password);
+	if (result) {
+		const cookie = await createSession(result.id, password);
+		res.send(JSON.stringify({
+			username: username,
+			session: cookie
+		}));
+	}
+	else {
+		res.status(400).send('{}');
+	}
 });
 
 
 expressApp.post('/login2', async (req: express.Request, res: express.Response) => {
-  const { username, password } = req.body;
+	const { username, password } = req.body;
 
-  res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Content-Type', 'application/json');
 
-  if (typeof username !== 'string' || typeof password !== 'string') {
-    res.status(400).send('{}');
-    return;
-  }
+	if (typeof username !== 'string' || typeof password !== 'string') {
+		res.status(400).send('{}');
+		return;
+	}
 
-  const result = await users.findOne({username: username, password:password});
-  if (result) {
-    const cookie = await createSession(result.id, password);
-    res.send(JSON.stringify({
-      username: username,
-      session: cookie
-    }));
-  }
-  else {
-    res.status(400).send('{}');
-  }
+	const result = await users.findOne({username: username, password:password});
+	if (result) {
+		const cookie = await createSession(result.id, password);
+		res.send(JSON.stringify({
+			username: username,
+			session: cookie
+		}));
+	}
+	else {
+		res.status(400).send('{}');
+	}
 });
 
 
 expressApp.get('/usernameTaken', async (req: express.Request, res: express.Response) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(`{"taken":${!!await users.findOne({username: req.query.username})}}`);
+	res.setHeader('Content-Type', 'application/json');
+	res.send(`{"taken":${!!await users.findOne({username: req.query.username})}}`);
 });
 
 
